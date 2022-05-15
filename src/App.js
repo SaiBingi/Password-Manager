04622ad04ccec90a7dcbe6b0682c03df481a6978
Sai Bingi <saibingi1208@gmail.com>
2022-05-15 11:35:07 +0000

    Password Manager Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==>  https://passwordmanagor.ccbp.tech
+
+import AddPassword from './components/AddPassword'
+
+import './App.css'
+
+const App = () => <AddPassword />
+
+export default App
